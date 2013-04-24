ae48dd3400d3569ef00cb65aec3480842a33a6b4
fredjiang <270130108@qq.com>
2013-04-24 19:41:22 +0800

    创建项目

---
+++ yutianDetect/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  yutianDetect
+//
+//  Created by fred on 4/24/13.
+//  Copyright (c) 2013 fred. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ yutianDetect/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  yutianDetect
+//
+//  Created by fred on 4/24/13.
+//  Copyright (c) 2013 fred. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
