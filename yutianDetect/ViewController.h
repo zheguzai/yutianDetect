f5092f253c860af45721d156c8a7b05df0972686
fredjiang <270130108@qq.com>
2013-04-27 19:29:34 +0800

    创建用于测试的环境

--- yutianDetect/ViewController.h
+++ yutianDetect/ViewController.h
@@ -9,20 +9,30 @@
 #import <UIKit/UIKit.h>
 #import <AVFoundation/AVFoundation.h>
 
-@interface ViewController : UIViewController<AVCaptureVideoDataOutputSampleBufferDelegate> {
+@interface ViewController : UIViewController<AVCaptureVideoDataOutputSampleBufferDelegate, UITextFieldDelegate> {
     AVCaptureSession *_captureSession;
     AVCaptureDevice *_captureDevice;
     AVCaptureVideoDataOutput *_videoOutput;
     AVCaptureVideoPreviewLayer *_videoPreviewLayer;
     
     IBOutlet UIImageView *_imageView_result;
-    IBOutlet UIImageView *_imageView_obj;
-    IBOutlet UIImageView *_imageView_scene;
+    IBOutlet UILabel *resultLabel;
+    IBOutlet UILabel *timeLabel;
+    IBOutlet UITextField *detectTypeTextField;
+    
+    IBOutlet UIScrollView *scrollView_obj;
+    IBOutlet UIScrollView *scrollView_scene;
+    
+    int currentIndex_obj;
+    int currentIndex_scene;
+    int detectType;
 }
 
 @property (nonatomic, readonly) AVCaptureSession *captureSession;
 @property (nonatomic, readonly) AVCaptureDevice *captureDevice;
 @property (nonatomic, readonly) AVCaptureVideoDataOutput *videoOutput;
 @property (nonatomic, readonly) AVCaptureVideoPreviewLayer *videoPreviewLayer;
+
+- (IBAction)clickDetectBtn:(id)sender;
 
 @end
